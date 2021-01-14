5b90ba3a881d12b83654547576acc81bdbef4747
Ангелина Статкевич <angelina.statkevich@gmail.com>
2021-01-14 19:57:59 +0300

    deleted some files

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -9,6 +9,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from './store/configureStore';
 
 const store = configureStore();
+
 const persistor = persistStore(store);
 
 ReactDOM.render(
